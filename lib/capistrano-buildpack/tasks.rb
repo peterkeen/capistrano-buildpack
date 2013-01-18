5765758ba901d032612b61946e53c1655fbf31e9
Pete Keen <peter.keen@bugsplat.info>
2013-01-17 17:56:43 -0800

    line up some lines, add an app_user setting

--- lib/capistrano-buildpack/tasks.rb
+++ lib/capistrano-buildpack/tasks.rb
@@ -9,10 +9,11 @@ end
 if Capistrano::Configuration.instance
   Capistrano::Configuration.instance.load do
 
-    _cset(:deploy_to) { "/apps/#{application}" }
+    _cset(:deploy_to)     { "/apps/#{application}" }
     _cset(:buildpack_url) { abort "Please specify the buildpack URL to use, set :buildpack_url, 'http://example.com/buildpack'" }
-    _cset(:base_port) { abort "Please specify a base port to use, set :base_port, 6500" }
-    _cset(:concurrency) { abort "Please specify a concurrency level to use, set :concurrency, 'web-1'" }
+    _cset(:base_port)     { abort "Please specify a base port to use, set :base_port, 6500" }
+    _cset(:concurrency)   { abort "Please specify a concurrency level to use, set :concurrency, 'web=1'" }
+    _cset(:app_user)      { user }
 
     _cset :foreman_export_path, "/etc/init"
     _cset :foreman_export_type, "upstart"
@@ -39,7 +40,7 @@ if Capistrano::Configuration.instance
       task :setup_env do
 
         set(:buildpack_hash) { Digest::SHA1.hexdigest(buildpack_url) }
-        set(:buildpack_path) { "#{shared_path}/buildpack-#{buildpack_hash}" }
+        set(:buildpack_path) { "#{shared_path}/.build/buildpack-#{buildpack_hash}" }
 
         default_run_options[:pty] = true
         default_run_options[:shell] = '/bin/bash'
@@ -50,9 +51,12 @@ if Capistrano::Configuration.instance
         sudo "mkdir -p #{deploy_to}"
         sudo "chown -R #{user} #{deploy_to}"
 
+
         run("[[ ! -e #{buildpack_path} ]] && git clone #{buildpack_url} #{buildpack_path}; exit 0")
         run("cd #{buildpack_path} && git fetch origin && git reset --hard origin/master")
-        run("mkdir -p #{shared_path}/build_cache")
+        run("mkdir -p #{shared_path}/.build/build_cache")
+        sudo "chown -R #{app_user} #{shared_path}"
+        sudo "chown -R #{user} #{shared_path}/.build"
 
       end
 
@@ -61,7 +65,7 @@ if Capistrano::Configuration.instance
       end
 
       task "compile" do
-        run("cd #{buildpack_path} && RACK_ENV=production bin/compile #{release_path} #{shared_path}/build_cache")
+        run("cd #{buildpack_path} && RACK_ENV=production bin/compile #{release_path} #{shared_path}/.build/build_cache")
 
         env_lines = []
         deploy_env.each do |k,v|
@@ -78,21 +82,21 @@ if Capistrano::Configuration.instance
         _ssl_key_path = exists?(:ssl_key_path) ? "SSL_KEY_PATH=#{ssl_key_path}" : ''
         _force_ssl = exists?(:force_ssl) ? "FORCE_SSL=#{force_ssl}" : ''
 
-        sudo "foreman export #{foreman_export_type} #{foreman_export_path} -d #{release_path} -l /var/log/#{application} -a #{application} -u #{user} -p #{base_port} -c #{concurrency}"
-        sudo "env #{_use_ssl} #{_ssl_cert_path} #{_ssl_key_path} #{_force_ssl} ADDITIONAL_DOMAINS=#{additional_domains.join(',')} BASE_DOMAIN=$CAPISTRANO:HOST$ nginx-foreman export nginx #{nginx_export_path} -d #{release_path} -l /var/log/apps -a #{application} -u #{user} -p #{base_port} -c #{concurrency}"
+        sudo "foreman export #{foreman_export_type} #{foreman_export_path} -d #{release_path} -l /var/log/#{application} -a #{application} -u #{app_user} -p #{base_port} -c #{concurrency}"
+        sudo "env #{_use_ssl} #{_ssl_cert_path} #{_ssl_key_path} #{_force_ssl} ADDITIONAL_DOMAINS=#{additional_domains.join(',')} BASE_DOMAIN=$CAPISTRANO:HOST$ nginx-foreman export nginx #{nginx_export_path} -d #{release_path} -l /var/log/apps -a #{application} -u #{app_user} -p #{base_port} -c #{concurrency}"
         sudo "service #{application} restart || service #{application} start"
         sudo "service nginx reload || service nginx start"
       end
 
     end
 
-    before "deploy", "buildpack:setup_env"
-    before "deploy:setup", "buildpack:setup_env"
-    before "deploy:setup", "buildpack:setup"
-    after  "deploy:setup", "buildpack:install_foreman_export_nginx"
-    before "deploy", "buildpack:setup"
+    before "deploy",                 "buildpack:setup_env"
+    before "deploy:setup",           "buildpack:setup_env"
+    before "deploy:setup",           "buildpack:setup"
+    after  "deploy:setup",           "buildpack:install_foreman_export_nginx"
+    before "deploy",                 "buildpack:setup"
     before "deploy:finalize_update", "buildpack:compile"
-    after  "deploy:create_symlink", "buildpack:foreman_export"
+    after  "deploy:create_symlink",  "buildpack:foreman_export"
 
     task 'remote' do
       buildpack.setup_env
